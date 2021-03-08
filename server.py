875bfa87d6cd99a40fa9e73ec5acf991752477bb
Fatima <fatima.hammouri@gmail.com>
2021-03-08 12:29:38 +0000

    initial commit

--- server.py
+++ server.py
@@ -18,9 +18,32 @@ def show_homepage():
 def show_form():
     return render_template('form.html')
 
+
 @app.route('/results')
 def show_results():
-    return render_template('results.html')
+    cheery = request.args.get('cheery')
+    honest = request.args.get('honest')
+    dreary = request.args.get('dreary')
+
+    if cheery:
+        msg = "this is a CHEERY message"
+    elif honest:
+        msg = "this is a very HONEST message"
+    elif dreary:
+        msg = "this is a DREARY message"
+   
+
+    return render_template('results.html', msg = msg)
+
+
+@app.route('/save_name', methods=["POST"])
+def save_name():
+    name = request.form.get('name')
+    session['name'] = name   
+    # the assignment is to render
+    # return render_template('homepage.html')
+    # but I think it makes sense to render ('form.html')
+    return render_template('form.html')
 
 
 if __name__ == "__main__":
